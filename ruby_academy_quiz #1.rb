f17c172aff4220b8819e3b4c38b213c1c0a2dd5f
ischenco <ischenco@gmail.com>
2021-07-05 11:58:19 +0000

    Quiz update with more comfotrable answers.

--- ruby_academy_quiz #1.rb
+++ ruby_academy_quiz #1.rb
@@ -1,10 +1,14 @@
+#Yukihiro Matsumoto
+
 "4" == 4 ? puts("TRUE") : puts("FALSE")
+#false
 
 x=0
 3.times do
   x+=1
 end
 puts x
+#3
 
 y=0
 3.times do
@@ -12,6 +16,7 @@ y=0
   x=y
 end
 puts x
+#3
 
 x=2
 if((x*3)/2) == (4+4-x-3)
@@ -19,6 +24,7 @@ if((x*3)/2) == (4+4-x-3)
 else
   puts "Did you?"
 end
+#Did you get it right?
 
 y=9
 x=10
@@ -31,9 +37,13 @@ puts "ALRIGHT NOW!"
 elese
 puts "Alrighty!"
 end
+#Alright now!
 
 847 < 846
 "847" < "846"
+#false
+#false
+#yes
 
 
 hash1= {shoes: "nike", "hat"=>"adidas", :hoodie => true}
@@ -43,7 +53,7 @@ if hash1==hash2
   puts "These hashes are the same!"
 else pusts"These hashes are not the same"
 end
-
+#These hashes are the same!
 
 
 arr = [15,7,18,5,12,8,5,1]
@@ -51,7 +61,7 @@ x=arr.index(5)
 y=arr[5]
 puts x
 puts y
-
+# x=3 y=8
 
 arr = [["test", "hello", "world"],["example","mem"]]
 puts [1][0]
@@ -63,26 +73,29 @@ string = "Ruby Courses Rock!"
 a,b,c,d =string[3], string[6], string[1], string[4]
 result = a + b + c + d + string.split[-1]
 puts result
-
+#you Rock!
 
 array = (1..10).to_a
 puts array.select(&:odd?)
-
+#1 3 5 7 9
 
 hash = {name: "Elizabeth", title:" Queen"}
 hash.merge({country: "United Kingdom"})
 puts hash
+#{:name=>"Elizabeth", :title=>" Queen"}
 
 hash = {name: "Alex", state:" healthy"}
 hash.merge!({disease: "Covid 19", state: "sick"})
 puts hash
+#{:name=>"Alex", :state=>"sick", :disease=>"Covid 19"}
 puts hash.value?("Alex")
+#true
 
 def report(name,*groceries,market:"Fidesco")
   puts "#{name} has bought #{groceries.join(", ")} at #{market}" 
 end
 report("Lena", "wine, shampoo", market:"Linela")
-
+#Lena has bought wine, shampoo at Linela
 
 
 
