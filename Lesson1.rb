547d1ee0e69b134c1556dff08d7a1691a2fbffbf
ischenco <ischenco@gmail.com>
2021-07-05 11:24:21 +0000

    Lesson 1 p1 - done

---
+++ Lesson1.rb
@@ -0,0 +1,21 @@
+string1="How do"
+string2="you do?"
+  puts string1+string2
+  puts string1 << string2
+  puts string1.concat(string2)
+
+  4.55**2
+  8.7**2
+  7.4**2
+
+
+(1..5).inject(:*)  
+(1..6).inject(:*)  
+(1..7).inject(:*)  
+(1..8).inject(:*)  
+
+(5..8).inject(:*)
+
+
+str3="Hello my friend"
+puts str3.delete(" ")
